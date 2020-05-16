1d0a2ff5010c2d5bb7508e749b607e184b6821a9
ashok41 <ashok.soundaram@gmail.com>
2020-05-15 21:53:39 -0700

    edit table

--- src/test-harness-tool/rules-processing/components/test-data/index.js
+++ src/test-harness-tool/rules-processing/components/test-data/index.js
@@ -1,9 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import { Row, Col, Button, Table } from 'react-bootstrap'
-import BootstrapTable from 'react-bootstrap-table-next';
-import cellEditFactory from 'react-bootstrap-table2-editor';
 import { useHistory, useLocation } from 'react-router-dom'
 import axios from 'axios'
+import RowEditable from './row-editable'
 import styles from './test-data.scss'
 import common from '../../../common/common.scss'
 
@@ -11,36 +10,12 @@ function TestData() {
   
   const history = useHistory()
   const location = useLocation()
+  	
+  const [dataLists, setDataLists] = useState(state)
   const {state} = location;
   
-  const columns = [{
-	  dataField: 'id',
-	  text: 'ID'
-	}, {
-	  dataField: 'applicationIdentity',
-	  text: 'Location Identity'
-	}, {
-	  dataField: 'bankDivision',
-	  text: 'Bank Division'
-	}, {
-	  dataField: 'productFamily',
-	  text: 'Product Family'
-	}, {
-	  dataField: 'productName',
-	  text: 'Product Name'
-	}, {
-	  dataField: 'barrowAmount',
-	  text: 'Borrow Amount'
-	}, {
-	  dataField: 'termFactor',
-	  text: 'Term Factor'
-	}, {
-	  dataField: 'riskFactor',
-	  text: 'Risk Factor'
-	}];
-    
   function handleSubmit() {
-	  axios.post('http://localhost:8081/expectedScenarios', state)
+	  axios.post('http://localhost:8081/expectedScenarios', dataLists)
 	  .then((response) => {
 		  const { data } = response
 		  history.push({
@@ -49,19 +24,34 @@ function TestData() {
 		})
 	  })
   }
+  function rowEdit(data, idx) {
+	  const obj = {...dataLists[idx], ...data}
+	  setDataLists([...dataLists.slice(0, idx), data, ...dataLists.slice(idx + 1)])
+  }
   return (
     <>
 	  <Row className={styles.padTop}>
 	    <Col md="12">
-		<BootstrapTable
-		  keyField="id"
-		  data={ state }
-		  columns={ columns }
-		  cellEdit={ cellEditFactory({
-			mode: 'click',
-			blurToSave: true
-		  }) }
-		/>
+		<Table responsive striped bordered hover size="sm">
+			  <thead>
+				<tr>
+				  <th>ID</th>
+				  <th>Location Identity</th>
+				  <th>Bank Division</th>
+				  <th>Product Family</th>
+				  <th>Product Name</th>
+				  <th>Borrow Amount</th>
+				  <th>Term Factor</th>
+				  <th>Risk Factor</th>
+				  <th>Action</th>
+				</tr>
+			  </thead>
+			  <tbody>
+				{dataLists.map((item, index) => (
+				  <RowEditable data={item} key={index} rowIndex={index} rowEdit={rowEdit} />
+				))}
+			  </tbody>
+		  </Table>
 		</Col>
 	  </Row>
 	  <Row className={styles.section}>
