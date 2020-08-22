dfb450ed909d8da3685c619a92f775df7f9e1553
ashok41 <ashok.soundaram@gmail.com>
2020-08-21 17:04:45 -0700

    update bread crumb in error page

--- src/test-harness-tool/error/index.js
+++ src/test-harness-tool/error/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Row, Col, Card, Breadcrumb } from 'react-bootstrap'
+import ProfileList from '../common/profile-list'
 import styles from './error.scss'
+import common from '../common/common.scss';
+
 	
 function Error() {
   return (
-    <div className={styles.section}>
-	  Unable to process your request. Please try again to give valid input.
-    </div>
+   <Row className={styles.section}>
+	<Col md="12">
+	 <Row>
+	   <Col md="9" className={common.listContainer}>
+		<Breadcrumb>
+		 <Breadcrumb.Item href="#/">Home</Breadcrumb.Item>
+		 <Breadcrumb.Item active>Error</Breadcrumb.Item>
+		</Breadcrumb>
+	   </Col>
+	   <Col md="3">
+		<ProfileList />
+	   </Col>	
+	 </Row>
+	 <Card>
+	 <Card.Body>
+	 Unable to process your request. Please try again to give valid input.
+	 </Card.Body>
+	 </Card>
+	</Col>
+   </Row>  
   );
 }
 
